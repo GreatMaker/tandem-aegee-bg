fb7d0f7fbfa854454e15e948ebea69de28dd4978
andrea.visinoni@gmail.com <andrea.visinoni@gmail.com@b511672e-5aac-2ec7-6eb5-85e35e2d69e8>
2012-07-20 14:40:37 +0000

    Page list file Page handling class Main view file Index file

---
+++ trunk/framework/page_class.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Page generation class
+ */
+class page_class
+{
+    public function __construct()
+    {
+        
+    }
+}
+?>
---
+++ trunk/framework/page_list.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Page list
+ */
+
+// pages
+define("HOME",      "home");
+
+// location
+$pages_list = array(HOME => array("page" => "main.php"));
+?>
